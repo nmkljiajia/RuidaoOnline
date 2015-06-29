21e929eeb9b59139de590e522cdae6404dc4eaa0
Y <Y@Y-Mac.local>
2015-06-29 10:39:37 +0800

    new

---
+++ RuidaoOnline/RuidaoOnline/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  RuidaoOnline
+//
+//  Created by Y on 15/6/29.
+//  Copyright (c) 2015年 Y. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
